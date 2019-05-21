68a22274f627ff463e4858b9032839e05f568f38
Logan Dempsey <logandempsey199@gmail.com>
2019-05-21 14:54:08 -0600

    third commit

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,7 +10,7 @@ class ContactForm extends Component{
         const message = document.getElementById('message').value;
         axios({
             method: "POST", 
-            url:"http://localhost:3002/send", 
+            url:"https://lrd-portfolio.herokuapp.com/contact/SEND", 
             data: {
                 name: name,   
                 email: email,  
